4a607867b1c00cc59b94a6165d8b2f3f1ad81cbe
Va test <arcticque421@gmail.com>
2023-10-25 18:51:28 +0300

    fix

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         OnTaskDoneListener listener = System.out::println;
-        OnTaskErrorListener listener1 = System.out::println;
+        OnTaskErrorListener errorTaskListener = System.out::println;
 
-        Worker worker = new Worker(listener, listener1);
+        Worker worker = new Worker(listener, errorTaskListener);
 
         worker.start();
     }
